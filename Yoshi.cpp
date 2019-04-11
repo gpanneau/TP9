80058db21ff2c6ea611df6addb3737814cf0a45f
gpanneau <guilhem.panneau@gmail.com>
2019-04-11 10:50:24 +0200

    Yoshi constructor patched

--- Yoshi.cpp
+++ Yoshi.cpp
@@ -6,9 +6,7 @@ std::string Yoshi::WhatAmI() const{
   return std::to_string(NbrOfCrests_)+" crested Yoshi";
 }
 
-Yoshi::Yoshi(int NumberOfCrests):Character(){
-  NbrOfCrests_=NumberOfCrests;
-}
+Yoshi::Yoshi(int NumberOfCrests):Character(),NbrOfCrests_(NumberOfCrests){}
 
 void Yoshi::Accelerate(){
   if (speed_<max_speed_){
