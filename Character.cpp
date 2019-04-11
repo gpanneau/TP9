ad81858f6d110ba0538edfd477bac049fe4f6528
gpanneau <guilhem.panneau@gmail.com>
2019-04-11 10:52:44 +0200

    Character constructor patched

--- Character.cpp
+++ Character.cpp
@@ -1,9 +1,6 @@
 #include "Character.h"
 
-Character::Character(){
-  speed_=0;
-  max_speed_=10;
-}
+Character::Character():speed_(0),max_speed_(10){}
 
 void Character::Accelerate(){
   if (speed_<max_speed_){
